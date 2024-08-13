39bed079eb6eb194d11c71e69662eb7987e8fc7f
levibanks <levibanks95@gmail.com>
2024-08-12 22:57:56 -0700

    Update ship speed

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,9 +35,13 @@ class AlienInvasion:
                 if event.key == pygame.K_RIGHT:
                     # Move the ship to the right.
                     self.ship.moving_right = True
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
